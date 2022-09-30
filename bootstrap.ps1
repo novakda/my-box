474cd8191d30d580e216b34675cc8823b094f980
Dan Novak <xhiris@gmail.com>
2022-09-30 16:45:16 -0700

    update to my github

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,4 @@
-iex "& { $(irm 'https://raw.githubusercontent.com/springcomp/my-box/main/bootstrap/powershell.ps1') }"
+iex "& { $(irm 'https://raw.githubusercontent.com/novakda/my-box/main/bootstrap/powershell.ps1') }"
 
-iex "& { $(irm 'https://raw.githubusercontent.com/springcomp/my-box/main/git.ps1') }"
-iex "& { $(irm 'https://raw.githubusercontent.com/springcomp/my-box/main/visual-studio-code.ps1') }"
+iex "& { $(irm 'https://raw.githubusercontent.com/novakda/my-box/main/git.ps1') }"
+iex "& { $(irm 'https://raw.githubusercontent.com/novakda/my-box/main/visual-studio-code.ps1') }"
