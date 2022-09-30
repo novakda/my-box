9fa36cb08d1dc5178902cdf5c1d17158077121fd
Dan Novak <xhiris@gmail.com>
2022-09-30 16:48:33 -0700

    update to my github

--- bootstrap/powershell.ps1
+++ bootstrap/powershell.ps1
@@ -13,7 +13,7 @@ Set-Alias -Name irs -Value Invoke-RemoteScript
 
 irs 'https://aka.ms/install-powershell.ps1' -daily
 
-irm 'https://raw.githubusercontent.com/springcomp/my-box/master/bootstrap/pwsh-core.ps1' `
+irm 'https://raw.githubusercontent.com/novakda/my-box/master/bootstrap/pwsh-core.ps1' `
 	-OutFile $env:TEMP\pwsh-core.ps1
 
 $pwsh = "$env:LOCALAPPDATA\Microsoft\powershell-daily\pwsh.exe"
